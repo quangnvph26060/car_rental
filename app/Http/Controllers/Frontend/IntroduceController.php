be1e84d660a31ceb9398d3845f4d7c31c60545c6
quangan010903@gmail.com <quangan010903@gmail.com>
2024-11-18 15:23:56 +0700

    update code

--- app/Http/Controllers/Frontend/IntroduceController.php
+++ app/Http/Controllers/Frontend/IntroduceController.php
@@ -3,31 +3,29 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Benefit;
 use App\Models\Car;
 use App\Models\Review;
 use App\Models\ServiceCommitment;
+use App\Models\SgoContact;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
 class IntroduceController extends Controller
 {
-    public function introduce(){
+    public function introduce()
+    {
 
         $reviews = Review::get();
-        $servicecommitments = ServiceCommitment::get();
         $use_service = Type::take(10)
-        ->with(['cars' => function($query) {
-            $query->select('type_id', 'price');  // Chọn các trường cần thiết
-        }])
-        ->get();
+            ->with(['cars' => function ($query) {
+                $query->select('type_id', 'price');  // Chọn các trường cần thiết
+            }])
+            ->get();
 
         $cars = Car::all();
-
-
-        // dd($use_service[5]->cars);
-
-
-        //  dd($reviews);
-        return view('frontend.pages.introduce', compact('reviews', 'servicecommitments', 'use_service', 'cars'));
+        $benefits = Benefit::where('status', 1)->latest()->take(4)->get();
+        $commitments = ServiceCommitment::latest()->take(3)->get();
+        return view('frontend.pages.introduce', compact('reviews', 'commitments', 'use_service', 'cars', 'benefits'));
     }
 }
--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -6,30 +6,22 @@ use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use App\Models\Car;
 use App\Models\Color;
+use App\Models\SgoContact;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    // public function list()
-    // {
-    //     return view('frontend.pages.list');
-    // }
-
-    // public function detail()
-    // {
-    //     return view('frontend.pages.detail');
-    // }
-
     public function product($slug = null)
     {
-        $totalCars = Car::count(); // Đếm tổng số xe
-        $limit = 6; // Số xe mỗi trang
+        $totalCars = Car::count();
+        $limit = 6;
         $maxPages = ceil($totalCars / $limit);
 
         if ($slug) {
             $product = Car::where('slug', $slug)->with('types', 'brands', 'carImages', 'color')->first();
-            return view('frontend.pages.product.detail', compact('slug', 'product' , 'totalCars', 'maxPages'));
+            $contact = SgoContact::first();
+            return view('frontend.pages.product.detail', compact('slug', 'product', 'totalCars', 'maxPages', 'contact'));
         }
 
         return view('frontend.pages.product.list', compact('slug'));
@@ -37,12 +29,13 @@ class ProductController extends Controller
 
     public function loadMoreCars(Request $request)
     {
-        $page = $request->get('page', 1);
+        $offset = $request->offset ?? 0;
         $limit = 6;
-
-        $cars = Car::orderBy('created_at', 'desc')->skip(($page - 1) * $limit)->take($limit)->get();
-
-
-        return response()->json($cars);
+        $products = Car::skip($offset)->take($limit)->get();
+        $hasMore = Car::count() > ($offset + $limit);
+        return response()->json([
+            'products' => $products,
+            'hasMore' => $hasMore,
+        ]);
     }
 }
