e0d4c72c5e0c3185dc414830ab164c95db2f30c3
quangan010903@gmail.com <quangan010903@gmail.com>
2024-11-22 11:15:47 +0700

    fix car

--- resources/views/frontend/pages/service/dichvu.blade.php
+++ resources/views/frontend/pages/service/dichvu.blade.php
@@ -28,7 +28,7 @@
                                 <p class="price mona-text-label">{{ number_format($car->price) }} VND</p>
                                 <div class="mona-except">
                                     <p>
-                                        {!! Str::words($car->description, 20, '[...]') !!}
+                                        {!! Str::words($car->description, 5, '[...]') !!}
                                     </p>
 
                                 </div>
@@ -84,7 +84,7 @@
                                 <p class="price mona-text-label">{{ number_format($car->price) }} VND</p>
                                 <div class="mona-except">
                                     <p>
-                                        {!! Str::words($car->description, 20, '[...]') !!}
+                                        {!! Str::words($car->description, 5, '[...]') !!}
                                     </p>
 
                                 </div>
