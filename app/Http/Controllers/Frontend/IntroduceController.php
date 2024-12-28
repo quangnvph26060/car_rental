82cb350bdc05d2b727bd19d6c0547fe44ce1b6ae
datdev <dnguyentien145@gmail.com>
2024-12-28 14:50:04 +0700

    update code

--- app/Http/Controllers/Frontend/IntroduceController.php
+++ app/Http/Controllers/Frontend/IntroduceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Album;
 use App\Models\Benefit;
 use App\Models\Car;
 use App\Models\Review;
@@ -25,6 +26,28 @@ class IntroduceController extends Controller
         $benefits = Benefit::where('status', 1)->latest()->take(4)->get();
         $commitments = ServiceCommitment::latest()->take(3)->get();
         $carsGallery = Car::take(8)->get();
-        return view('frontend.pages.introduce', compact('reviews', 'commitments', 'use_service', 'cars', 'benefits','carsGallery'));
+        $images = $this->getFirstImages();
+        return view('frontend.pages.introduce', compact('reviews', 'commitments', 'use_service', 'cars', 'benefits', 'carsGallery', 'images'));
+    }
+
+    public function getFirstImages()
+    {
+        // Lấy tất cả các album
+        $albums = Album::all();
+
+        $images = [];
+
+        // Duyệt qua từng album để lấy ảnh đầu tiên
+        foreach ($albums as $album) {
+            // Giả sử albums là một mảng JSON chứa các URL ảnh
+            $albumImages = $album->album;
+
+            // Kiểm tra xem có ảnh không và lấy ảnh đầu tiên
+            if (is_array($albumImages) && count($albumImages) > 0) {
+                $images[] = $albumImages[0]; // Lấy ảnh đầu tiên
+            }
+        }
+
+        return $images;
     }
 }
